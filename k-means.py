775a4e474eb3bf4c217266da4452281272c8cca3
Callum Hall <cal.hall.040892@gmail.com>
2025-09-19 14:34:28 +0100

    Implement code changes to enhance functionality and improve performance

--- k-means.py
+++ k-means.py
@@ -2,3 +2,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+df = pd.read_excel(r'C:\Users\Callum\Documents\ML Researcher\k-means_customer\k-means_customer_segmentation\data\Online Retail.xlsx')
+df.head()
