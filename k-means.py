606f5080691efcaf8db32728825502def5097014
Callum Hall <cal.hall.040892@gmail.com>
2025-09-22 20:45:51 +0100

    Update k-means.py

--- k-means.py
+++ k-means.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import skilearn.
 
 df = pd.read_excel(r'C:\Users\Callum\Documents\ML Researcher\k-means_customer\k-means_customer_segmentation\data\Online Retail.xlsx')
 df.head()
